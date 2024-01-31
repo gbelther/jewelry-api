f7db6a0edd23e5fd3b2d711f648753b8f20ae519
Gabriel Belther <belthergabriel@gmail.com>
2024-01-30 22:37:53 -0300

    feat: define port

--- src/server.ts
+++ src/server.ts
@@ -106,6 +106,7 @@ router.get("/products/:id", (request, response) => {
 
 app.use(router);
 
-app.listen(3001, () => {
-  console.log("Server is running on port 3001");
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
