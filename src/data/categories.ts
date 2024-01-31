8a4b3ffeb37d49cdea9d2430a80487365b02b7d0
Gabriel Belther <belthergabriel@gmail.com>
2024-01-30 22:24:33 -0300

    feat: tratamento de filtros na listagem

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -1,18 +1,22 @@
 export const categories = [
   {
     id: "1",
+    key: "aneis",
     name: "Aneis",
   },
   {
     id: "2",
+    key: "aliancas",
     name: "Alianças",
   },
   {
     id: "3",
+    key: "colares",
     name: "Colares",
   },
   {
     id: "4",
+    key: "braceletes",
     name: "Braceletes",
   },
 ];
--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,10 @@ const app = express();
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+  setTimeout(next, 2000);
+});
+
 const router = Router();
 
 router.get("/categories", (request, response) => {
@@ -19,11 +23,75 @@ router.get("/best-sellers", (request, response) => {
   }, 1000);
 });
 
+const sortByRelevance = (
+  prev: (typeof products)[number],
+  next: (typeof products)[number]
+) => {
+  return -1;
+};
+
+const sortByPriceAsc = (
+  prev: (typeof products)[number],
+  next: (typeof products)[number]
+) => {
+  return prev.price - next.price;
+};
+
+const sortByPriceDesc = (
+  prev: (typeof products)[number],
+  next: (typeof products)[number]
+) => {
+  return next.price - prev.price;
+};
+
+const sortByAlphabeticalAsc = (
+  prev: (typeof products)[number],
+  next: (typeof products)[number]
+) => {
+  if (prev.name < next.name) {
+    return -1;
+  }
+  if (prev.name > next.name) {
+    return 1;
+  }
+  return 0;
+};
+
+const sortByAlphabeticalDesc = (
+  prev: (typeof products)[number],
+  next: (typeof products)[number]
+) => {
+  if (prev.name > next.name) {
+    return -1;
+  }
+  if (prev.name < next.name) {
+    return 1;
+  }
+  return 0;
+};
+
+const sortStrategy = {
+  relevance: sortByRelevance,
+  "price-asc": sortByPriceAsc,
+  "price-desc": sortByPriceDesc,
+  "alphabetical-asc": sortByAlphabeticalAsc,
+  "alphabetical-desc": sortByAlphabeticalDesc,
+};
+
 router.get("/products", (request, response) => {
-  const { categoryId } = request.query;
-  const productsFiltered = products.filter(
-    (product) => product.categoryId === categoryId
-  );
+  const { categoryId, categoryKey, sort } = request.query;
+  const productsFiltered = products
+    .filter((product) =>
+      categoryId ? product.categoryId === categoryId : true
+    )
+    .filter((product) => {
+      if (!categoryKey) return true;
+      const category = categories.find(
+        (category) => category.key === categoryKey
+      );
+      return product.categoryId === category.id;
+    })
+    .sort(sortStrategy[sort as keyof typeof sortStrategy]);
   response.status(200).json(productsFiltered);
 });
 
